f9542c915b584caa8a70a05c38fa00b5f4f44ca7
godwinchibyk <godwin.chibyk@gmail.com>
2024-10-30 15:31:44 +0100

    adjust sidebar link text

--- components/Layouts/DashboardLayout/SideBarSection/SideBarLinkItem.tsx
+++ components/Layouts/DashboardLayout/SideBarSection/SideBarLinkItem.tsx
@@ -17,7 +17,7 @@ export const SideBarLinkItem = ({
   return (
     <Link
       href={route}
-      className={`flex flex-row items-center space-x-5 rounded-lg p-5 transition-all hover:bg-bgColor/80 ${
+      className={`flex flex-row items-center space-x-5 rounded-lg p-3 md:p-5 transition-all hover:bg-bgColor/80 ${
         isLinkActive
           ? "bg-primaryColorLight hover:bg-primaryColorLight/90"
           : "bg-transparent"
@@ -26,7 +26,7 @@ export const SideBarLinkItem = ({
     >
       {linkIcon}
       <p
-        className={`text-base text-pretty capitalize text-textColor/90 tracking-wide font-[family-name:var(--font-medium)] tex ${
+        className={`text-base  text-nowrap capitalize text-textColor/90 tracking-wide font-[family-name:var(--font-medium)]${
           isLinkActive ? "font-[family-name:var(--font-semibold)]" : ""
         }`}
       >
